4a7c58343c96d225a23bf8941a5371b341fc9507
shivammourya17 <153920006+shivammourya17@users.noreply.github.com>
2025-08-27 02:11:29 +0530

    final

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // ⚡ Important: Fixes white page & MIME type issues on Netlify
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
+    
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
